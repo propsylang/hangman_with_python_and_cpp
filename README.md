725b18ab2d71e2ee45c36a1ba237cf4e2d08fe98
propsylang <propsylang@gmail.com>
2020-09-06 19:32:15 +0900

    added description

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hangman_with_python_and_cpp
 hangman tutorial in python and c++
+
+ハングマンのルール
+
--- hangman.py
+++ hangman.py
@@ -1,1 +1,83 @@
-print("hello")
+import random
+
+def printword_with_holes():
+    pass
+
+print("-----------HANGMAN-------------")
+cleared = False
+tries = 7
+word_ans = "default"
+letters_guessed = []
+while not cleared and tries > 0:
+    inp = input("Type in letter or word:").upper()
+    if len(inp) is 1 and inp.isalpha():
+        if inp in word_ans:
+            pass
+    elif len(inp) is len(word_ans) and inp.isalpha():
+        if inp is word_ans:
+            cleared = True
+        else:
+            print("wrong word!")
+    else:
+        print("invalid input")
+
+
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ / \\  |
+      |
+=========''','''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ / \\  |
+      |
+===RIP===''']
+
+
